593be192c73a7d426bfbecd108dfbdff00c590cb
vikash6865 <prajapativikash0000@gmail.com>
2018-12-29 20:39:35 +0530

    svgs updated

--- source/index.blade.php
+++ source/index.blade.php
@@ -36,11 +36,11 @@
         </nav>
         <div class="cover-category">
             <div class="photography category">
-                <a href="/#">@include('_svgs/instagram')</a>
+                <a href="/#">@include('_svgs/photo-camera')</a>
                 <h1>Photography</h1>
             </div>
             <div class="development category">
-                <a href="/#">@include('_svgs/instagram')</a>
+                <a href="/#">@include('_svgs/code1')</a>
                 <h1>Development</h1>
             </div>
         </div>
