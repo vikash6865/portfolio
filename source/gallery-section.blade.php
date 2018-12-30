f5468743b08dd5c3db1ba2f93a739a4588afdadb
vikash6865 <prajapativikash0000@gmail.com>
2018-12-30 20:34:04 +0530

    implementing grid

--- source/gallery-section.blade.php
+++ source/gallery-section.blade.php
@@ -1,6 +1,13 @@
 <div class="gallery-section">
-        <h1 class="section-title gallery">Recent Photos</h1>
-        <div class="polaroid-container">
-            
+        {{-- <h1 class="section-title gallery">Recent Photos</h1> --}}
+        <div class="grid-container">
+            <div class="grid-item one">1</div>
+            <div class="grid-item two">2</div>
+            <div class="grid-item three">3</div>
+            <div class="grid-item four">4</div>
+            <div class="grid-item">5</div>
+            <div class="grid-item">6</div>
+            <div class="grid-item">7</div>
+            <div class="grid-item">8</div>
         </div>
 </div>
