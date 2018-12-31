9ac3c4f0583930e2fcde057ba7e1fb5b8054772a
vikash6865 <prajapativikash0000@gmail.com>
2018-12-31 18:57:41 +0530

    cover fix, grid fix

--- source/gallery-section.blade.php
+++ source/gallery-section.blade.php
@@ -1,7 +1,9 @@
 <div class="gallery-section">
         {{-- <h1 class="section-title gallery">Recent Photos</h1> --}}
         <div class="grid-container">
-            <div class="grid-item one">1</div>
+            <div class="grid-item one">
+                <img src="/assets/images/dome.jpg" class="gallery-thumbnail">
+            </div>
             <div class="grid-item two">2</div>
             <div class="grid-item three">3</div>
             <div class="grid-item four">4</div>
