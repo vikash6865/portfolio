5c824ef6e7318b45984f19903d5a053ea997b903
vikash6865 <prajapativikash0000@gmail.com>
2019-01-06 13:10:23 +0530

    images optimised

--- source/gallery-section.blade.php
+++ source/gallery-section.blade.php
@@ -8,22 +8,22 @@
                 <img src="/assets/images/gallery/landscape/landscape-2.jpg" class="gallery-thumbnail">
             </div>
             <div class="grid-item three">
-                <img src="/assets/images/gallery/landscape/landscape-13.jpg" class="gallery-thumbnail">
+                <img src="/assets/images/gallery/landscape/landscape-11.jpg" class="gallery-thumbnail">
             </div>
             <div class="grid-item four">
-                <img src="/assets/images/gallery/landscape/landscape-9.jpg" class="gallery-thumbnail">
+                <img src="/assets/images/gallery/landscape/landscape-14.jpg" class="gallery-thumbnail">
             </div>
             <div class="grid-item five">
-                <img src="/assets/images/gallery/landscape/landscape-6.jpg" class="gallery-thumbnail">
+                <img src="/assets/images/gallery/landscape/landscape-12.jpg" class="gallery-thumbnail">
             </div>
             <div class="grid-item six">
-                    <img src="/assets/images/gallery/potrait/potrait-4.jpg" class="gallery-thumbnail">
+                    <img src="/assets/images/gallery/potrait/potrait-1.jpg" class="gallery-thumbnail">
             </div>
             <div class="grid-item seven">
-                    <img src="/assets/images/gallery/landscape/landscape-10.jpg" class="gallery-thumbnail">
+                    <img src="/assets/images/gallery/landscape/landscape-13.jpg" class="gallery-thumbnail">
             </div>
             <div class="grid-item eight">
-                    <img src="/assets/images/gallery/landscape/landscape-12.jpg" class="gallery-thumbnail">
+                    <img src="/assets/images/gallery/landscape/landscape-15.jpg" class="gallery-thumbnail">
             </div>
         </div>
 </div>
