4d86ecad658a034d994c674442370fdb99aa9e58
vikash6865 <prajapativikash0000@gmail.com>
2019-01-06 12:21:31 +0530

    position of place name in stories changed

--- source/stories-section.blade.php
+++ source/stories-section.blade.php
@@ -2,8 +2,10 @@
     <h1 class="section-title stories">New Stories</h1>
     <div class="stories-container">
         <div class="story-card">
-            <div class="place-name">place</div>
-            <img src="/assets/images/gallery/landscape/landscape-9.jpg" alt="here is an image">
+            <div class="story-img">
+                    <div class="place-name">place</div>
+                <img src="/assets/images/gallery/landscape/landscape-9.jpg" alt="here is an image">
+            </div>
             <h3 class="story-title">Title</h3>
             <h5 class="story-date">22 July 2018</h5>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut perspiciatis libero aperiam aspernatur? Optio, sed! Ab minima voluptatibus atque suscipit, provident labore quia eveniet consectetur voluptatum, hic impedit in officia.</p>
@@ -12,8 +14,10 @@
                 </div>
         </div>
         <div class="story-card">
-                <div class="place-name">place</div>
-                <img src="./assets/images/gallery/landscape/landscape-10.jpg" alt="here is an image">
+            <div class="story-img">
+                    <div class="place-name">place</div>
+                    <img src="./assets/images/gallery/landscape/landscape-10.jpg" alt="here is an image">
+                </div>
                 <h3 class="story-title">Title</h3>
                 <h5 class="story-date">22 July 2018</h5>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut perspiciatis libero aperiam aspernatur? Optio, sed! Ab minima voluptatibus atque suscipit, provident labore quia eveniet consectetur voluptatum, hic impedit in officia.</p>
@@ -22,8 +26,10 @@
                     </div>
         </div>
         <div class="story-card">
-                <div class="place-name">place</div>
-                <img src="./assets/images/gallery/landscape/landscape-7.jpg" alt="here is an image">
+            <div class="story-img">
+                    <div class="place-name">place</div>
+                    <img src="./assets/images/gallery/landscape/landscape-7.jpg" alt="here is an image">
+                </div>
                 <h3 class="story-title">Title</h3>
                 <h5 class="story-date">22 July 2018</h5>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut perspiciatis libero aperiam aspernatur? Optio, sed! Ab minima voluptatibus atque suscipit, provident labore quia eveniet consectetur voluptatum, hic impedit in officia.</p>
